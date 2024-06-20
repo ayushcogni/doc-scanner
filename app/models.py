18c66603b49721a4aa197498d5749c444578bc63
Ayush Shrivatsav <ayush.s@cogniquest.ai>
2024-06-20 15:57:47 +0530

    new column added

--- app/models.py
+++ app/models.py
@@ -1,15 +1,22 @@
-from flask_sqlalchemy import SQLAlchemy
+# app/models.py
 
-db = SQLAlchemy()
+from datetime import datetime
+from app import db
 
 class FileMetadata(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    file_path = db.Column(db.String(255), unique=True, nullable=False)
-    file_name = db.Column(db.String(100), nullable=False)
-    checksum = db.Column(db.String(64), nullable=False)
-    upload_time = db.Column(db.DateTime, nullable=False)
-    validation_status = db.Column(db.String(20), nullable=False)
-    process_status = db.Column(db.String(20), nullable=False)
+    __tablename__ = 'uploadDocumentsV1'
 
-    def __repr__(self):
-        return f'<FileMetadata {self.file_name}>'
+    id = db.Column(db.Integer, primary_key=True)
+    userid = db.Column(db.String(50))
+    docName = db.Column(db.String(255))
+    docType = db.Column(db.String(50))
+    publish = db.Column(db.Boolean, default=False)
+    upload_time = db.Column(db.DateTime, default=datetime.utcnow)
+    process_st_time = db.Column(db.DateTime)
+    process_en_time = db.Column(db.DateTime)
+    process_stats = db.Column(db.String(50))
+    filepath = db.Column(db.String(255))
+    metainfo = db.Column(db.Text)
+    project_name = db.Column(db.String(255))
+    subscription = db.Column(db.String(255))
+    batch_name = db.Column(db.String(255))
