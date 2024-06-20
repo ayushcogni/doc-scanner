eb610a0e71b1d324f95bdc57d8a19c0d4fec5b1a
Ayush Shrivatsav <ayush.s@cogniquest.ai>
2024-06-20 11:46:09 +0530

    folders added

--- app/config.py
+++ app/config.py
@@ -3,6 +3,6 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///file_metadata.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    INPUT_FOLDER = 'input'
-    ERRORS_FOLDER = 'errors'
-    PROCESSED_FOLDER = 'processed'
+    INPUT_FOLDER = '/home/azureuser/doc_scanner/doc-scanner/inpput'
+    ERRORS_FOLDER = '/home/azureuser/doc_scanner/doc-scanner/error'
+    PROCESSED_FOLDER = '/home/azureuser/doc_scanner/doc-scanner/processed'
