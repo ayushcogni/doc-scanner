bed44f09458423ab8b24f343462b8b190411526f
Ayush Shrivatsav <ayush.s@cogniquest.ai>
2024-06-20 11:48:01 +0530

    fix context

--- main.py
+++ main.py
@@ -4,5 +4,6 @@ from app.utils import monitor_and_process_files
 app = create_app()
 
 if __name__ == '__main__':
-    monitor_and_process_files()
+    with app.app_context():
+        monitor_and_process_files()
     app.run(debug=True)
